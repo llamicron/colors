fca61298a194b3cc703824a4b6cd7c8355790ec0
Luke Sweeney <luke@thesweeneys.org>
2017-10-25 17:29:34 -0500

    push to pi

--- colors/app.py
+++ colors/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 jinja_options = app.jinja_options.copy()
@@ -13,10 +13,29 @@ jinja_options.update(dict(
 ))
 app.jinja_options = jinja_options
 
+def hex_to_rgb(hex_string):
+    if not isinstance(hex_string, str):
+        raise TypeError("Hex must be a string")
+    if not '#' in hex_string:
+        raise ArgumentError("Hex must star with '#'")
+    return tuple(int(hex_string[1:][i:i+2], 16) for i in (0, 2 ,4))
+
+def updateLEDs(color):
+    # Update LEDs when they come in the mail, lmao
+    print(color)
+    return True
 
 @app.route("/")
-def hello():
+def index():
     return render_template("index.html")
 
+@app.route('/update', methods=['POST'])
+def colorReceiver():
+    data = request.get_json()
+    color = hex_to_rgb(data['color'])
+    # send color to LEDs
+    updateLEDs(color)
+    return "True"
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0")
--- colors/static/colors.js
+++ colors/static/colors.js
@@ -9,11 +9,24 @@ var colors = new Vue({
     clickColorPicker() {
       // This is pretty hack-y but the html5 color picker doesn't play nice with hidden elements
       document.getElementById("colorPicker").click();
+    },
+
+    updateLEDs(color) {
+      axios.post('/update', {
+        color: color
+      })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    },
+  },
+
+  watch: {
+    color: function (color) {
+      this.updateLEDs(color);
     }
   }
-  // watch: {
-  //   color: function (color) {
-  //     // update LEDs / send color to python
-  //   }
-  // }
 });
