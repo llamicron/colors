dee4312d756a3a0ee9893c408716c0d23dbdbf1b
Luke Sweeney <luke@thesweeneys.org>
2017-12-01 13:52:15 -0600

    colors 2.0

--- colors/static/colors.js
+++ colors/static/colors.js
@@ -1,32 +1,64 @@
+var colorWheel = new ColorWheel("#colorwheel");
+colorWheel.bindData(1);
+// window.setInterval(function () {
+//   console.log(colorWheel.getVisibleMarkers().data()[0].color);
+// }, 2000);
+function HSVtoRGB(h, s, v) {
+  var r, g, b, i, f, p, q, t;
+  if (arguments.length === 1) {
+    s = h.s, v = h.v, h = h.h;
+  }
+  i = Math.floor(h * 6);
+  f = h * 6 - i;
+  p = v * (1 - s);
+  q = v * (1 - f * s);
+  t = v * (1 - (1 - f) * s);
+  switch (i % 6) {
+    case 0: r = v, g = t, b = p; break;
+    case 1: r = q, g = v, b = p; break;
+    case 2: r = p, g = v, b = t; break;
+    case 3: r = p, g = q, b = v; break;
+    case 4: r = t, g = p, b = v; break;
+    case 5: r = v, g = p, b = q; break;
+  }
+  return {
+    r: Math.round(r * 255),
+    g: Math.round(g * 255),
+    b: Math.round(b * 255)
+  };
+}
+
 var colors = new Vue({
   el: "#colors",
 
   data: {
-    color: '#647eff'
+    color: colorWheel.getVisibleMarkers().data()[0].color
   },
 
   methods: {
-    clickColorPicker() {
-      // This is pretty hack-y but the html5 color picker doesn't play nice with hidden elements
-      document.getElementById("colorPicker").click();
-    },
-
     updateLEDs(color) {
       axios.post('/update', {
         color: color
-      })
-      .then(response => {
+      }).then(response => {
         console.log(response);
-      })
-      .catch(error => {
+      }).catch(error => {
         console.log(error);
-      });
+      })
+    }
+  },
+
+  computed: {
+    RGBColor() {
+      return HSVtoRGB(this.color);
     },
   },
 
   watch: {
-    color: function (color) {
-      this.updateLEDs(color);
+    color: {
+      handler: function (color) {
+        this.updateLEDs(this.RGBColor);
+      },
+      deep: true
     }
   }
 });
